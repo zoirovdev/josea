11e5995644cd2cf10afc11206ffac1254a436b73
zoirovdev <abboszoirov17@gmail.com>
2025-05-24 23:50:08 +0500

    update ui

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom'
 import { Box } from '@mui/material'
 import Main from './components/Main.jsx'
-
+import Navbar from './components/Navbar.jsx'
 
 
 function App() {
   return (
     <Box>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Main/>}/>
       </Routes>
